465f91b997769ddc82daefcc90d5751a7d5d5522
Jeehun Choi <hobbit0717@gmail.com>
2021-08-14 21:50:55 -0400

    Initial Commit

---
+++ CitySightsApp/CitySightsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySightsAppApp.swift
+//  CitySightsApp
+//
+//  Created by JH on 2021-08-14.
+//
+
+import SwiftUI
+
+@main
+struct CitySightsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
