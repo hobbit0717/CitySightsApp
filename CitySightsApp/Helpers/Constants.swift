a6cafa9f994309a3e63ce4a545ee395320a2887f
Jeehun Choi <hobbit0717@gmail.com>
2021-08-22 18:57:55 -0400

    - remove yelp api key

--- CitySightsApp/Helpers/Constants.swift
+++ CitySightsApp/Helpers/Constants.swift
@@ -8,7 +8,8 @@
 import Foundation
 
 struct Constants {
-    static var apiKey = "661pgF9DgZH39XoFTo1blCAVi_30JE6HnCyuW4vbHYDYUr33kZXfN7fQ_xDtvlRlPQAoqRjruJs395a6_coSTKyTM7wo3haXN8FS1jA7xUAWuCSpCskkD_URR4QZYXYx"
+    // Please use your own apiKey
+    static var apiKey = ""
     static var apiUrl = "https://api.yelp.com/v3/businesses/search"
     static var restaurantsKey = "restaurants"
     static var sightsKey = "arts"
